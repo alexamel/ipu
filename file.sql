1a19bf00fb1a17a6520fe4e7973c18768e2d412d
Zach Eagle <mazaxakep@gmail.com>
2014-06-23 18:37:16 +0400

    Обновил БД и форму

--- file.sql
+++ file.sql
@@ -36,7 +36,7 @@ CREATE TABLE `add` (
   `status` tinyint(1) NOT NULL,
   `date` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +45,7 @@ CREATE TABLE `add` (
 
 LOCK TABLES `add` WRITE;
 /*!40000 ALTER TABLE `add` DISABLE KEYS */;
-INSERT INTO `add` VALUES (1,'ddddd','dfd','dfd','dfd',34343,'dfdf','df','dfd','fdfdfd',1,'2014-06-23'),(2,'dfsdfsdfs','sdfs','dfsdf','dsfds',45454,'dfsdfs','dfsd','fsdf','sdfdsfs',1,'2014-06-23'),(3,'fsdfsd','dsfs','fsdf','sdfs',334,'dfs','fsd','fsd','fsdfsd',1,'2014-06-23'),(4,'sdfsdfds','sdf','sdfdsf','sdfsd',3434,'3dfsd','fsdf','sdfsd','fsdf',1,'2014-06-23'),(5,'gdfgdfgd','dfd','sdfs','dfs',454,'dfd','gdfg','df','gdfgdf',1,'2014-06-23'),(6,'fsdfsdfs','dfd','dfd','fdfd',3443,'dfsdfs','dfsd','fsd','fsd',1,'2014-06-23'),(7,'sdfdsfsd','fd','fsdf','sdfsdf',4534,'sdfds','fsd','fds','fsdf',1,'2014-06-23'),(8,'gdfgdfgdf','asdf','adsf','sdfasd',454,'fdgvdg','df','gdf','gdfgdf',1,'2014-06-23'),(9,'fsdfsd','sdfds','fsdf','sdfsd',4534,'dfsdfsd','fsd','fsd','fsd',1,'2014-06-23'),(10,'dfgdfgdfgdfgdf','sdfds','fsdfsd','fdsfsd',3453453,'dsfgdfg','df','gdfgdfg','dfgdfgdfg',1,'2014-06-23');
+INSERT INTO `add` VALUES (1,'Хочу быть с Вами.','Семен','Шишкин','sema',12,'Тушино, ул.Восьмого Вагона 12',' Рост','12м','13м',0,'2014-06-23');
 /*!40000 ALTER TABLE `add` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -65,7 +65,7 @@ CREATE TABLE `chat` (
   PRIMARY KEY (`id`),
   KEY `chat_user_id` (`user_id`),
   CONSTRAINT `chat_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -74,7 +74,7 @@ CREATE TABLE `chat` (
 
 LOCK TABLES `chat` WRITE;
 /*!40000 ALTER TABLE `chat` DISABLE KEYS */;
-INSERT INTO `chat` VALUES (1,'cerf','oki',2,'2014-06-23'),(2,'lol','vcb',2,'2014-06-23'),(3,'lol','vce',2,'2014-06-23'),(4,'fsdfsdfds','cer',2,'2014-06-23'),(5,'Показания в программе  Показания на счетчике  Проблема: asdf','cerh',2,'2014-06-23'),(6,'сука блять сука','yu',2,'2014-06-23');
+INSERT INTO `chat` VALUES (1,'Отлючили электроэнергию вчера','Сегодня будет',2,'2014-06-23'),(2,'Все еще не дали.','',2,'2014-06-23');
 /*!40000 ALTER TABLE `chat` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -92,13 +92,13 @@ CREATE TABLE `meter` (
   `firm` varchar(255) NOT NULL,
   `model` varchar(255) NOT NULL,
   `met_id` varchar(255) NOT NULL,
-  `record_id` int(11) NOT NULL,
+  `record_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `meter_record_id` (`record_id`),
   KEY `meter_user_id` (`user_id`),
   CONSTRAINT `meter_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `meter_ibfk_2` FOREIGN KEY (`record_id`) REFERENCES `record` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -107,7 +107,7 @@ CREATE TABLE `meter` (
 
 LOCK TABLES `meter` WRITE;
 /*!40000 ALTER TABLE `meter` DISABLE KEYS */;
-INSERT INTO `meter` VALUES (2,9,'dfdf','df','dfd','fdfdfd',3),(3,10,'dfsdfs','dfsd','fsdf','sdfdsfs',4),(4,11,'dfs','fsd','fsd','fsdfsd',5),(5,12,'3dfsd','fsdf','sdfsd','fsdf',6),(6,13,'3dfsd','fsdf','sdfsd','fsdf',7),(7,14,'3dfsd','fsdf','sdfsd','fsdf',8),(8,15,'3dfsd','fsdf','sdfsd','fsdf',9),(9,16,'3dfsd','fsdf','sdfsd','fsdf',10),(10,17,'3dfsd','fsdf','sdfsd','fsdf',11),(11,18,'3dfsd','fsdf','sdfsd','fsdf',12),(12,19,'3dfsd','fsdf','sdfsd','fsdf',13),(13,2,'Мо','dfgв','ав','ав',5),(14,20,'dfd','gdfg','df','gdfgdf',14),(15,21,'dfsdfs','dfsd','fsd','fsd',15),(16,22,'fdgvdg','df','gdf','gdfgdf',16),(17,23,'sdfds','fsd','fds','fsdf',17),(18,24,'dsfgdfg','df','gdfgdfg','dfgdfgdfg',18),(19,25,'dfsdfsd','fsd','fsd','fsd',19);
+INSERT INTO `meter` VALUES (3,2,'Москва, Тверская 21','Сенхайзер','34g','12met123',2);
 /*!40000 ALTER TABLE `meter` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -123,7 +123,7 @@ CREATE TABLE `record` (
   `data` int(11) NOT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -132,7 +132,7 @@ CREATE TABLE `record` (
 
 LOCK TABLES `record` WRITE;
 /*!40000 ALTER TABLE `record` DISABLE KEYS */;
-INSERT INTO `record` VALUES (1,12344,'2014-06-11 04:19:20'),(2,50729,'2014-06-23 15:16:40'),(3,65570,'2014-06-23 15:17:14'),(4,18008,'2014-06-23 15:43:07'),(5,53403,'2014-06-23 15:43:57'),(6,70491,'2014-06-23 15:45:29'),(7,74427,'2014-06-23 15:45:29'),(8,77954,'2014-06-23 15:45:29'),(9,86300,'2014-06-23 15:45:29'),(10,7449,'2014-06-23 15:45:29'),(11,77667,'2014-06-23 15:45:29'),(12,75599,'2014-06-23 15:45:29'),(13,47205,'2014-06-23 15:45:29'),(14,20687,'2014-06-23 17:12:13'),(15,40588,'2014-06-23 17:13:21'),(16,77754,'2014-06-23 17:17:10'),(17,95734,'2014-06-23 17:17:10'),(18,82950,'2014-06-23 17:20:28'),(19,33814,'2014-06-23 17:20:28');
+INSERT INTO `record` VALUES (1,3433,'2014-06-23 11:30:20'),(2,234,'2014-06-23 19:29:40');
 /*!40000 ALTER TABLE `record` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -151,7 +151,7 @@ CREATE TABLE `user` (
   `sername` varchar(255) NOT NULL,
   `role` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -160,7 +160,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'11',1,'Иван','Ивановушкин','disp'),(2,'22',2,'Андрей','Дибрович','user'),(4,'123',0,'Василий','Иванов','user'),(5,'sdfsd',4534,'dfs','fsdf','user'),(6,'sdfsd',4534,'dfs','fsdf','user'),(7,'dfd',34343,'dfd','dfd','user'),(8,'dfd',34343,'dfd','dfd','user'),(9,'dfd',34343,'dfd','dfd','user'),(10,'dsfds',45454,'sdfs','dfsdf','user'),(11,'sdfs',334,'dsfs','fsdf','user'),(12,'sdfsd',3434,'sdf','sdfdsf','user'),(13,'sdfsd',3434,'sdf','sdfdsf','user'),(14,'sdfsd',3434,'sdf','sdfdsf','user'),(15,'sdfsd',3434,'sdf','sdfdsf','user'),(16,'sdfsd',3434,'sdf','sdfdsf','user'),(17,'sdfsd',3434,'sdf','sdfdsf','user'),(18,'sdfsd',3434,'sdf','sdfdsf','user'),(19,'sdfsd',3434,'sdf','sdfdsf','user'),(20,'dfs',454,'dfd','sdfs','user'),(21,'fdfd',3443,'dfd','dfd','user'),(22,'sdfasd',454,'asdf','adsf','user'),(23,'sdfsdf',4534,'fd','fsdf','user'),(24,'fdsfsd',3453453,'sdfds','fsdfsd','user'),(25,'sdfsd',4534,'sdfds','fsdf','user');
+INSERT INTO `user` VALUES (1,'11',1,'Иван','Ивановушкин','disp'),(2,'22',2,'Андрей','Дибрович','user');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -173,4 +173,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-06-23 18:12:31
+-- Dump completed on 2014-06-23 18:35:17
